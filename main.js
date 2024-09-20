6073d791b071036f96617140fdd5b3463ebfb730
Kleber Fellipe <133381521+KleberFellipe@users.noreply.github.com>
2024-09-20 12:12:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Oi Lucas");
+console.log("Oi João");
+
+
+var titulo = document.querySelector("h1");
+titulo.textContent = "Kleber Nutrição";
+
+var trPaulo = document.querySelector("#Paulo")
+
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
